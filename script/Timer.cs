0f667506fbbdaeabd517bcbcc2f0ee11fa589570
shamam kafri <111068539+shamam99@users.noreply.github.com>
2023-07-16 22:18:00 +0300

    Add files via upload

---
+++ script/Timer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Timer : MonoBehaviour
+{
+        [Header("component")]
+        public TextMeshProUGUI timerText;
+
+        [Header("Timer Settings")]
+        public float currentTime;
+        public bool countDown;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
+        timerText.text = currentTime.ToString();
+        
+    }
+}
